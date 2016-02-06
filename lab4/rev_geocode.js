a82ee1bf2287250be653ebd3b8064d36c2e8736d
amdudda <tk0654wm@minneapolis.edu>
2016-02-05 22:25:47 -0600

    Testing reverse geocode lookup - I actually go the thing working!!!

--- lab4/rev_geocode.js
+++ lab4/rev_geocode.js
@@ -17,23 +17,26 @@ function initMap() {
 // can we display the result of a geocode lookup of Minneapolis?  Yes, yes we can!
     var nyc = {lat:40.714224,lng:-73.961452}
     var mpls = {lat: 44.972583, lng: -93.283667};
-    geocoder.geocode({'location': {lat:40.714224,lng:-71.961452}}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {         // when we go to update the game, we need a "while not OK" loop.
-            if (results[3]) {
-                /*for (var i= 0; i<results.length; i++)
-                {
-                    var newPara = document.createElement("p");
-                    newPara.innerText = "#" + i + ": " + results[i].formatted_address;
-                    rP.appendChild(newPara);
-                }*/
-                rP.innerText =  results[3].formatted_address;
+    var danversMN = {lat:45.281999, lng:-95.751928}
+    var cities = [nyc,mpls,danversMN];
+    for (var c=0; c<cities.length; c++) {
+        geocoder.geocode({'location': cities[c]}, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {         // when we go to update the game, we need a "while not OK" loop.
+                if (results[1]) {
+                    for (var i = 0; i < results.length; i++) {
+                        var newPara = document.createElement("p");
+                        newPara.innerText = "#" + i + ": " + results[i].formatted_address;
+                        rP.appendChild(newPara);
+                    }
+                    //rP.innerText =  results[3].formatted_address;
+                } else {
+                    rP.innerText = 'No results found';
+                }
             } else {
-                rP.innerText = 'No results found';
+                rP.innerText = 'Geocoder failed due to: ' + status;
             }
-        } else {
-            rP.innerText = 'Geocoder failed due to: ' + status;
-        }
-    });
+        });
+    }
 
 }
 
