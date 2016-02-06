c19612059b9a8a0d8ff9ef4f6469e7c4cc40ad4e
amdudda <tk0654wm@minneapolis.edu>
2016-02-05 22:16:15 -0600

    Testing reverse geocode lookup - I actually go the thing working!!!

---
+++ lab4/rev_geocode.js
@@ -0,0 +1,39 @@
+/**
+ * Created by amdudda on 2/5/16.
+ */
+var geocoder;
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {lat: 40.731, lng: -73.997}
+    });// also hide the map since we're not actually using it
+    document.getElementById('map').style.display = "none";
+
+    // set up our geocoder and the area to display results.
+    geocoder = new google.maps.Geocoder;
+    var rP = document.getElementById("resultsPane");
+
+// can we display the result of a geocode lookup of Minneapolis?  Yes, yes we can!
+    var nyc = {lat:40.714224,lng:-73.961452}
+    var mpls = {lat: 44.972583, lng: -93.283667};
+    geocoder.geocode({'location': {lat:40.714224,lng:-71.961452}}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {         // when we go to update the game, we need a "while not OK" loop.
+            if (results[3]) {
+                /*for (var i= 0; i<results.length; i++)
+                {
+                    var newPara = document.createElement("p");
+                    newPara.innerText = "#" + i + ": " + results[i].formatted_address;
+                    rP.appendChild(newPara);
+                }*/
+                rP.innerText =  results[3].formatted_address;
+            } else {
+                rP.innerText = 'No results found';
+            }
+        } else {
+            rP.innerText = 'Geocoder failed due to: ' + status;
+        }
+    });
+
+}
+
